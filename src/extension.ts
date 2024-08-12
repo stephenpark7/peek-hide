3089cbca53c8fe63985db9ba5c3631aab07d977e
Stephen Park <spark0843@gmail.com>
2024-08-12 00:57:27 -0400

    use code from peekhiddenfiles extension as base

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,102 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "peek-hide" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('peek-hide.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS CODE');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension.hideFiles', hide),
+		vscode.commands.registerCommand('extension.showFiles', show),
+		vscode.commands.registerCommand('extension.toggleFiles', toggle)
+	);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+function hide() {
+	updateConfiguration(true);
+}
+
+function show() {
+	updateConfiguration(false);
+}
+
+function toggle() {
+	updateConfiguration();
+}
+
+const configKey = 'files.exclude';
+
+function updateConfiguration(value?: boolean) {
+	const settings: { uri?: vscode.Uri, excludes: any, target: vscode.ConfigurationTarget }[] = [
+		...getGeneralSettings(),
+		...getFoldersSettings()
+	];
+
+	if (value === undefined) {
+		value = !getCurrentState(settings);
+	}
+
+	for (const setting of settings) {
+		vscode.workspace.getConfiguration(undefined, setting.uri)
+			.update(configKey, setState(setting.excludes, value), setting.target);
+	}
+}
+
+function getGeneralSettings() {
+	var config = vscode.workspace.getConfiguration()
+		.inspect(configKey);
+
+	const settings = [];
+
+	if (config && config.globalValue) {
+		settings.push({
+			excludes: config.globalValue,
+			target: vscode.ConfigurationTarget.Global
+		});
+	}
+
+	if (config && config.workspaceValue) {
+		settings.push({
+			excludes: config.workspaceValue,
+			target: vscode.ConfigurationTarget.Workspace
+		});
+	}
+
+	return settings;
+}
+
+function getFoldersSettings() {
+	const folders = vscode.workspace.workspaceFolders;
+	if (!folders) {
+		return [];
+	}
+
+	const settings = [];
+	for (const folder of folders) {
+		const config = vscode.workspace.getConfiguration(undefined, folder.uri)
+			.inspect(configKey);
+
+		if (!config || !config.workspaceFolderValue) {
+			continue;
+		}
+
+		settings.push({
+			uri: folder.uri,
+			excludes: config.workspaceFolderValue,
+			target: vscode.ConfigurationTarget.WorkspaceFolder
+		});
+	}
+
+	return settings;
+}
+
+function getCurrentState(settings: { uri?: vscode.Uri, excludes: any, target: vscode.ConfigurationTarget }[]) {
+	return settings.some(s => Object.values(s.excludes).some(v => v === true));
+}
+
+function setState(excludes: any, value: boolean) {
+	for (const key of Object.keys(excludes)) {
+		if (typeof excludes[key] === 'boolean') {
+			excludes[key] = value;
+		}
+	}
+	return excludes;
+}
